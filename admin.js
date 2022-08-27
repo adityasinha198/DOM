9833b9b800acbfd62917584c0d09b85581195769
adityasinha198 <adityasinha198@gmail.com>
2022-08-28 01:14:52 +0530

    Chat Project

---
+++ admin.js
@@ -0,0 +1,51 @@
+
+const express=require('express');
+
+const fs=require('fs');
+
+const router=express.Router();
+
+let data1;
+
+router.get('/',(req,res,next)=>{
+    fs.readFile('./routes/data.js','UTF8',(err,data)=>{
+      
+        res.send(
+            `<div>
+            <p>${data}</p>
+            <form  action="/admin/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')"
+         method='POST'>
+         <input id="message" name="message" type="text" placeholder="message">
+         <input type="hidden" name="username" id="username">
+         <button type="submit">SEND</button>
+        
+        </form>
+        </div>`)
+        
+    })
+    
+   
+    // res.send(
+    //     `<div>
+    //     <p>${data1}</p>
+    //     <form  action="/admin/" onsubmit="document.getElementById('username').value=localStorage.getItem('username')"
+    //  method='POST'>
+    //  <input id="message" name="message" type="text" placeholder="message">
+    //  <input type="hidden" name="username" id="username">
+    //  <button type="submit">SEND</button>
+    
+    // </form>
+    // </div>`)
+
+});
+router.post('/',(req,res,next)=>{
+    
+     fs.appendFile('./routes/data.js',`  ${req.body.username}:${req.body.message}  `, (err)=>{
+        console.log('Done writing')
+     })
+    //console.log(data);
+    console.log(`{${req.body.username}:${req.body.message}}`)
+    res.redirect('/admin')
+});
+
+module.exports=router;
